185104169b9d8a7f6fc92dda6f2dd582f124f849
Saverio Tufano <saverio.tufano@gmail.com>
2023-10-13 22:08:27 +0100

    modelling_classification.py code refactoring

--- modelling_classification.py
+++ modelling_classification.py
@@ -42,6 +42,7 @@ def tune_classification_model_hyperparameters(model_class_obj: Type, parameters_
     # Train the best model on the test dataset and evaluate performance
     best_model.fit(X_test, y_test)
     y_pred = best_model.predict(X_test)
+
     # calculate performance metrics
     precision = precision_score(y_test, y_pred, average=None)
     recall = recall_score(y_test, y_pred, average=None)
@@ -57,10 +58,6 @@ def tune_classification_model_hyperparameters(model_class_obj: Type, parameters_
     model_info = {"best hyperparameters": best_hyperparams, "Precision": precision,
                   "Recall": recall, "Accuracy": accuracy}
 
-    #print(f"Accuracy: {accuracy:.2f}")
-    #print("Confusion Matrix:\n", conf_matrix)
-    print(model_class_obj)
-    print(model_info)
     return model_info
 
 
@@ -73,6 +70,7 @@ def save_model(model, model_filename: str, folder_path: str, model_info: dict):
             - folder path
             - dictionary containing the summary of the model perfomance on the dataset
     """
+
     full_model_path = folder_path + model_filename
 
     if os.path.isdir(folder_path) == False:
@@ -89,19 +87,28 @@ def save_model(model, model_filename: str, folder_path: str, model_info: dict):
     
 
 def evaluate_all_models(model_list: list , parameter_grid_list: list, X_train, X_test, y_train, y_test):
-    # decision trees, random forests, and gradient boosting
-    # It's extremely important to apply your tune_regression_model_hyperparameters function
-    # to each of these to tune their hyperparameters before evaluating them
-    # Save the model, hyperparameters, and metrics in a folder named after the model class.
-    # For example, save your best decision tree in a folder called models/regression/decision_tree.
-    # rand_forest = RandomForestRegressor()
-    # grad_boost = GradientBoostingRegressor()
+    """
+        Evaluates all models in the model list.
+        Each model is evaluated according to a grid list by tune_regression_model_hyperparameters function
+        Parameters:
+            a list of model classes (and NOT instances of a class)
+            a grid of model parameters
+            test and training datasets (features and labels)
+        Returns:
+            saves the best model for each class in a folder
+    """
+
     for index, model in enumerate(model_list):
         print(model, parameter_grid_list[index])
         model_performance = tune_classification_model_hyperparameters(model, parameter_grid_list[index],
                                                                   X_train, X_test, y_train, y_test)
-        save_model(model, model_filename='best_'+model.__name__, folder_path='models/classification/logistic_regression/'+model.__name__+'/',
-                   model_info=model_performance)
+        
+        # define model naming strategy and saving folder path
+        model_filename = 'best_'+model.__name__
+        folder_path = 'models/classification/logistic_regression/'+model.__name__+'/'
+
+        save_model(model, model_filename=model_filename,
+                   folder_path=folder_path, model_info=model_performance)
 
 
 def find_best_model(search_directory = './models/classification/logistic_regression'):
@@ -112,6 +119,7 @@ def find_best_model(search_directory = './models/classification/logistic_regress
             - a dictionary of its hyperparameters
             - a dictionary of its performance metrics.
     """
+
     # Define the file extension you want to search for
     file_extension = '*.json'
 
@@ -136,35 +144,42 @@ def find_best_model(search_directory = './models/classification/logistic_regress
 
 
 if __name__ == "__main__":
+    data_path = "./airbnb-property-listings/tabular_data/clean_tabular_data.csv"
+
     # load the previously cleaned data
-    df = pd.read_csv("./airbnb-property-listings/tabular_data/clean_tabular_data.csv")
-    # select labels and features
+    df = pd.read_csv(data_path)
+    
+    # define labels and features
     features, labels = load_airbnb(df, label="Category", numeric_only=True)
-    # rescale the features
+
+    # features scaling  
     scaler = StandardScaler()
     scaled_features = scaler.fit_transform(features) # Fit and transform the data
+
     # split in train, validation, test sets
     X_train, X_test, y_train, y_test = train_test_split(scaled_features, labels, test_size=0.3)
     X_validation, X_test, y_validation, y_test = train_test_split(X_test, y_test, test_size=0.5)
 
-    model_list = [LogisticRegression, RandomForestClassifier, GradientBoostingClassifier]
+    model_list = [LogisticRegression, # model 1
+                  RandomForestClassifier, # model 2
+                  GradientBoostingClassifier] # model 3
     
     param_grid_list = [
-                {
+                { # model 1
                 'penalty': ['l1', 'l2'],            # Regularization type ('l1' for L1, 'l2' for L2)
                 'C': [0.001, 0.01, 0.1, 1.0, 10.0], # Inverse of regularization strength
                 'solver': ['liblinear', 'saga'],    # Solver algorithms (suitable for small to medium datasets)
                 'max_iter': [100, 200, 300],        # Maximum number of iterations for solver convergence
                 'class_weight': [None, 'balanced'], # Class weight ('balanced' adjusts weights based on class distribution)
                 },
-                {
+                { # model 2
                 'n_estimators': [10, 50, 100],         # Number of trees in the forest
                 'max_depth': [None, 10, 20, 30],      # Maximum depth of the trees
                 'min_samples_split': [2, 5, 10],      # Minimum number of samples required to split a node
                 'min_samples_leaf': [1, 2, 4],        # Minimum number of samples required at each leaf node
                 'class_weight': [None, 'balanced']
                 },
-                {
+                { # model 3
                 'n_estimators': [50, 100, 200],       # Number of boosting stages to be used
                 'learning_rate': [0.01, 0.1, 0.2],   # Step size shrinkage used to prevent overfitting
                 'max_depth': [3, 4, 5],              # Maximum depth of the individual trees
@@ -172,10 +187,8 @@ if __name__ == "__main__":
                 }
                 ]
 
-    #model_info = tune_classification_model_hyperparameters(model_list, param_grid,
-    #                                                       X_train, X_test, y_train, y_test,
-    #                                                       validation_accuracy="accuracy", random_state = 1)
-    #print(model_info)
+    # evaluate all models in the model list according to the parameters in the grid
+    # for each model type, save the best
+    evaluate_all_models(model_list, param_grid_list, X_train, X_test, y_train, y_test)
     
-    #evaluate_all_models(model_list, param_grid_list, X_train, X_test, y_train, y_test)
     best_model, best_performance, best_hyperparams = find_best_model()
